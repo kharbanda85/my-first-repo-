b96b35f152ea902001a27f55c938db3f6521e54e
kharbanda85 <dr.hitesh85@gmail.com>
2021-04-05 17:45:50 +0530

    added README.md

--- README.md
+++ README.md
@@ -1,26 +1,14 @@
-# Sean Kross
+## The Unix Workbench course assignment
+*by Johns Hopkins University on 
+[coursera.org](https://www.coursera.org/).*
+\n**Description**: make a program called *guessinggame.sh*. This 
+program should$
+\n**Make date**:
 
-### Geography
 
-I live in the city of Baltimore, in the state of Maryland, in the United States
-of America.
 
-### Reading
-
-Three of my favorite books are:
-
-- *Mindstorms* by Seymour Papert
-- *Welcome to the Monkey House* by Kurt Vonnegut
-- *Persepolis* by Marjane Satrapi
-
-### Food
-
-Last night I dreamt about eating in these restaurants:
-
-1. Linger in Denver.
-2. Azura in Jerusalem.
-3. Gemma in New York City.
-
-### Contact
-
-The best way to get in touch with me is [on Twitter](https://twitter.com/seankross).
+## 
+The Unix Workbench course assignment
+*by Johns Hopkins University on [coursera.org](https://www.coursera.org/).*
+\n**Description**: make a program called *guessinggame.sh*. This program should continuously ask the user to guess the number of files in the current directory, until they guess the correct number. The user is informed if their guess is too high or too low. Once the user guesses the correct number of files in the current directory they should be congratulated.
+\n**Make date**: 
---
+++ guessinggame.sh
@@ -0,0 +1,10 @@
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="2">
+            <File firstVisibleLine="156" xOffset="0" scrollWidth="1656" startPos="4761" endPos="4755" selMode="0" lang="Python" encoding="-1" filename="C:\Users\hkharba\Desktop\Hunting Priortization Aanlytic\Daily_code\Daily_scoring code.py" backupFilePath="" originalFileLastModifTimestamp="-1563038141" originalFileLastModifTimestampHigh="30874402" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="6357072" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="255" xOffset="0" scrollWidth="1656" startPos="7804" endPos="7540" selMode="0" lang="Python" encoding="-1" filename="C:\Users\hkharba\Desktop\Hunting Priortization Aanlytic\Monthly_code\Data Transformation &amp; Model.py" backupFilePath="" originalFileLastModifTimestamp="729830715" originalFileLastModifTimestampHigh="30874431" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="1144258560" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="840" startPos="430" endPos="430" selMode="0" lang="Shell" encoding="-1" filename="C:\Users\hkharba\my-first-repo\guessinggame.sh" backupFilePath="C:\Users\hkharba\AppData\Roaming\Notepad++\backup\guessinggame.sh@2021-04-05_171706" originalFileLastModifTimestamp="639893405" originalFileLastModifTimestampHigh="30878225" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="50463234" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
